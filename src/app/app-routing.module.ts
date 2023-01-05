cb62330451de38d4067c691613811b186fe16e4e
Charity K. Mutembei <100141458+Charity-Mutembei@users.noreply.github.com>
2023-01-05 10:25:29 +0300

    Feature: create and style login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:LoginComponent}, 
+  {path: 'Sign in', component: SignInComponent}, 
+  {path: 'Dashboard', component: DashBoardComponent}, 
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { DashBoardComponent } from './components/dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignInComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule,
